fcbf82447ca512c52bd1ead622ed94e24b6fc4e6
Ryan-Dunne <c00263405@itcarlow.ie>
2024-02-28 23:31:52 +0000

    Records can now be sent to company table

--- Car Rental/AddCompany.js
+++ Car Rental/AddCompany.js
@@ -5,13 +5,8 @@ function helpMenu()
 
 function generateNumber()
 {
-    const LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; //String of Letters
-    let letterLength = LETTERS.length;  //Length of String
-    
-    let startingLetter = LETTERS.charAt(Math.floor(Math.random() * letterLength)) //Starting letter will be randomly chosen from 0 to letterLength value
-    
     let number = Math.floor((Math.random() * 10000000) + 1) //Random number generated from 1 to 1000000
-    document.getElementById("id").value = startingLetter+number; //combines then to make a Company ID, sent to form on page load
+    document.getElementById("id").value = number; //combines then to make a Company ID, sent to form on page load
 }   
 
 function resetForm()
--- Car Rental/AddCompany.php
+++ Car Rental/AddCompany.php
@@ -3,15 +3,15 @@ include 'db.inc';
 date_default_timezone_set("UTC");
 echo "The details sent down are: <br>";
 
-echo "Your Company unique ID is: " .$POST['id']. "<br>"
+echo "Your Company unique ID is: " . $_POST['id'] . "<br>";
 echo "Company name: " . $_POST['company'] . "<br>";
 echo "Address: " . $_POST['address'] . "<br>";
 echo "Company Balance: " . $_POST['balance'] . "<br>";
 echo "Company Credit: " . $_POST['credit'] . "<br>";
 
 
-$sql = "Insert into company (CompanyName,Address,Balance,CreditLimit)
-VALUES ('$_POST[company]','$_POST[address]','$_POST[balance]','$_POST[credit]')";
+$sql = "Insert into company (CompanyID,CompanyName,Address,Balance,CreditLimit)
+VALUES ('$_POST[id]','$_POST[company]','$_POST[address]','$_POST[balance]','$_POST[credit]')";
 
 if (!mysqli_query($con,$sql))
 {

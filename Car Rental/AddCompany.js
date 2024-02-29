6288a92fd6fd21e23b7010f7d1f1cff4e195f50d
(Student  C00263405) Ryan Dunne <C00263405@setu.ie>
2024-02-29 10:54:09 +0000

    adjusted banner

--- Car Rental/AddCompany.js
+++ Car Rental/AddCompany.js
@@ -3,16 +3,3 @@ function helpMenu()
     alert ("Help")
 }
 
-function generateNumber()
-{
-    let number = Math.floor((Math.random() * 10000000) + 1) //Random number generated from 1 to 1000000
-    document.getElementById("id").value = number; //combines then to make a Company ID, sent to form on page load
-}   
-
-function resetForm()
-{
-    document.getElementById("company").value = null;
-    document.getElementById("address").value = null;
-    document.getElementById("balance").value = null;
-    document.getElementById("credit").value = null;
-}

92a09a9d75bb1dac2a1aab51bfa0ca2f20e63517
(Student  C00263405) Ryan Dunne <C00263405@setu.ie>
2024-02-29 10:04:14 +0000

    updated form

--- Car Rental/AddCompany.php
+++ Car Rental/AddCompany.php
@@ -5,14 +5,15 @@ echo "The details sent down are: <br>";
 
 echo "Your Company unique ID is: " . $_POST['id'] . "<br>";
 echo "Company name: " . $_POST['company'] . "<br>";
-echo "Company Tele: " . $_POST['tele'] . "<br>";
 echo "Address: " . $_POST['address'] . "<br>";
-echo "Company Balance: " . $_POST['balance'] . "<br>";
-echo "Company Credit: " . $_POST['credit'] . "<br>";
+echo "Company Tele: " . $_POST['tele'] . "<br>";
+echo "Company Web Address: " . $_POST['web'] . "<br>";
+echo "Company Email Address: " . $_POST['email'] . "<br>";
+echo "Company Credit Limit: " . $_POST['credit'] . "<br>";
 
 
-$sql = "Insert into company (CompanyID,CompanyName,Address,Balance,CreditLimit)
-VALUES ('$_POST[id]','$_POST[company]','$_POST[address]','$_POST[balance]','$_POST[credit]')";
+$sql = "Insert into company (CompanyID,CompanyName,Address,Telephone,WebAddress,EmailAddress,CreditLimit)
+VALUES ('$_POST[id]','$_POST[company]','$_POST[address]','$_POST[tele]','$_POST[web]','$_POST[email]','$_POST[credit]')";
 
 if (!mysqli_query($con,$sql))
 {

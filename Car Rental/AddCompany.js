af4bbaa3be23b3ef7faccbda212f00fce48d26f9
(Student  C00263405) Ryan Dunne <C00263405@setu.ie>
2024-02-29 16:15:08 +0000

    validation & stuff

--- Car Rental/AddCompany.js
+++ Car Rental/AddCompany.js
@@ -1,5 +1,20 @@
 function helpMenu()
 {
-    alert ("Help")
+    alert ("A Company's Web Address is not required\n New Companies should start at 1000 credit limit")
 }
+
+function confirmSubmit()    //function called when form is about to be submit
+{
+
+        if(confirm("Are you sure you want to submit this form?") == false)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+   
+}
+
 

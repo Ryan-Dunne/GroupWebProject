94d6b7b6f4a557afdca11925d20266efc1e5a8e3
Ryan-Dunne <c00263405@itcarlow.ie>
2024-03-04 16:50:34 +0000

    Changed form design & added a min for credit limit

--- Car Rental/AddCompany.php
+++ Car Rental/AddCompany.php
@@ -1,17 +1,9 @@
 <?php
 include 'db.inc';
 date_default_timezone_set("UTC");
-echo "The details sent down are: <br>";
+// echo "The details sent down are: <br>";
 
 $companyID = generateID();
-
-echo "Your Company unique ID is: " . $companyID . "<br>";
-echo "Company name: " . $_POST['company'] . "<br>";
-echo "Address: " . $_POST['address'] . "<br>";
-echo "Company Tele: " . $_POST['tele'] . "<br>";
-echo "Company Web Address: " . $_POST['web'] . "<br>";
-echo "Company Email Address: " . $_POST['email'] . "<br>";
-echo "Company Credit Limit: " . $_POST['credit'] . "<br>";
 
 
 $sql = "Insert into company (CompanyID,CompanyName,Address,Telephone,WebAddress,EmailAddress,CreditLimit)
@@ -22,7 +14,7 @@ if (!mysqli_query($con,$sql))
     die ("An Error in the SQL Query: " . mysqli_error($con) );
 }
 
-echo "<br>A record has been added for " . $_POST['company'];
+
 
 mysqli_close($con);
 
@@ -41,9 +33,3 @@ function generateID()
 }
 
 ?>
-
-<form action = "AddCompany.html" method = "POST">
-<br>
-    <input type="submit" value = "Return to Add Company Page"/>
-
-</form>
